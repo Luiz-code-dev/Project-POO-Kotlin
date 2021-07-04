15e4a82acebeda4cfee812bb0215e4a8db5821a2
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-04 16:31:04 -0300

    Inicio Project

---
+++ src/main/kotlin/digitalBank/People.kt
@@ -0,0 +1,17 @@
+package digitalBank
+
+class People {
+
+    var nome: String = "Luiz"
+
+    var cpf: String = "123.456.789-10"
+
+}
+
+fun main() {
+
+    val Luiz = People()
+
+    println(Luiz.nome)
+    println(Luiz.cpf)
+}
